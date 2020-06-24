a2d8307c3b941455026b335eb7b9f2050f2fa89d
drianclark <dreiclark@gmail.com>
2020-06-24 14:55:49 +0100

    added wheel as setup.py requirement

--- setup.py
+++ setup.py
@@ -18,5 +18,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3'
+    python_requires='>=3',
+    setup_requires=["wheel"]
 )
